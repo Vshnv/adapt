700a03e011aa154c07bcf5b1e8af96ac9ed77b07
Vaishnav Anil <vaishnav.nair@horizonind.org>
2023-08-24 01:58:26 +0530

    Updated demo app

--- app/src/main/java/io/github/vshnv/adapt/MainActivity.kt
+++ app/src/main/java/io/github/vshnv/adapt/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.vshnv.adapt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.viewbinding.ViewBinding
 import io.github.vshnv.adapt.databinding.ActivityMainBinding
 import io.github.vshnv.adapt.databinding.LayoutTextItemBinding
@@ -10,32 +11,43 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val adapter = adapt<String> {
-        create {
-            ViewSource.BindingViewSource(LayoutTextItemBinding.inflate(layoutInflater), ViewBinding::getRoot)
-        }.bind { model, layoutTextItemBinding ->
-            layoutTextItemBinding.tvTest.text = model
-        }
-        itemEquals { s, s2 -> s == s2 }
-        contentEquals { s, s2 -> s == s2 }
-    }
+    private lateinit var adapter: AdaptAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        adapter = adapt {
+            create {
+                ViewSource.BindingViewSource(LayoutTextItemBinding.inflate(layoutInflater), ViewBinding::getRoot)
+            }.bind { model, layoutTextItemBinding ->
+                layoutTextItemBinding.tvTest.text = model
+            }
+        }
         binding.rvTest.adapter = adapter
-        adapter.submitData(listOf("a", "b", "c", "d"))
+        GlobalScope.launch {
+            val list = mutableListOf<Int>()
+            for (i in (1..1000)) {
+                delay(1000)
+                list.add(i)
+                if (i % 9 == 0) {
+                    list.shuffle()
+                }
+                withContext(Dispatchers.Main) {
+                    adapter.submitData(list.map { it.toString() })
+                }
+            }
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        adapter.submitData(listOf("a", "b", "c", "f", "e ", "t"))
     }
 }
