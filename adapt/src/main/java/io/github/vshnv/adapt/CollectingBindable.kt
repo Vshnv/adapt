764e2542f5eca8d02c296b2473d44dd0c1f92077
Vaishnav Anil <vaishnav.nair@horizonind.org>
2023-08-24 01:58:16 +0530

    Fixed invalid cast in CollectingBindable

--- adapt/src/main/java/io/github/vshnv/adapt/CollectingBindable.kt
+++ adapt/src/main/java/io/github/vshnv/adapt/CollectingBindable.kt
@@ -1,10 +1,23 @@
 package io.github.vshnv.adapt
 
+import android.view.View
+import java.lang.RuntimeException
+
 class CollectingBindable<T, V>(val creator: () -> ViewSource<V>): Bindable<T, V> {
     var bindView: ((T, Any) -> Unit)? = null
         private set
 
     override fun bind(bindView: (T, V) -> Unit) {
-        this.bindView = { a, b -> bindView(a, b as V) }
+        this.bindView = { a, b -> bindView(a, resolveSourceParam(b)) }
+    }
+
+    private fun resolveSourceParam(item: Any): V {
+        return when (item) {
+            is ViewSource.BindingViewSource<*> -> item.binding as V
+            is ViewSource.SimpleViewSource<*> -> item.view as V
+            else -> {
+                throw RuntimeException("Invalid ViewSource found!")
+            }
+        }
     }
 }
