a8652c08315362b534b2aea92221596faf6cdd9d
Vaishnav Anil <vaishnav.nair@horizonind.org>
2024-03-12 00:51:21 +0530

    Implemented automatic ViewBinding inflation

--- adapt/build.gradle.kts
+++ adapt/build.gradle.kts
@@ -46,6 +46,7 @@ android {
 }
 
 dependencies {
+    compileOnly("androidx.databinding:viewbinding:8.3.0")
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- adapt/src/main/java/io/github/vshnv/adapt/dsl/AdaptScope.kt
+++ adapt/src/main/java/io/github/vshnv/adapt/dsl/AdaptScope.kt
@@ -1,6 +1,9 @@
 package io.github.vshnv.adapt.dsl
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import java.lang.reflect.Method
 
 interface AdaptScope<T: Any> {
     fun itemEquals(checkEquality: (data: T, otherData: T) -> Boolean)
@@ -8,4 +11,22 @@ interface AdaptScope<T: Any> {
     fun defineViewTypes(mapToViewType: (data: T, position: Int) -> Int)
     fun <V: Any> create(createView: (parent: ViewGroup) -> ViewSource<V>): Bindable<T, V>
     fun <V: Any> create(viewType: Int, createView: (parent: ViewGroup) -> ViewSource<V>): Bindable<T, V>
+
+}
+
+inline fun <T : Any, reified V : ViewBinding> AdaptScope<T>.create(): Bindable<T, V> = create {
+    ViewSource.BindingViewSource(inflateViewBinding(V::class.java, it), ViewBinding::getRoot)
+}
+
+private val inflateMethodCache = mutableMapOf<Class<out ViewBinding>, Method>()
+fun <T : ViewBinding> inflateViewBinding(bindingClass: Class<T>, parent: ViewGroup): T {
+    val layoutInflater = LayoutInflater.from(parent.context)
+    return inflateMethodCache.getOrPut(bindingClass) {
+        bindingClass.getMethod(
+            "inflate",
+            LayoutInflater::class.java,
+            ViewGroup::class.java,
+            Boolean::class.javaPrimitiveType
+        )
+    }.invoke(null, layoutInflater, parent, false) as T
 }
